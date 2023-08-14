81c963ff84515bb797af41774914ecb33e20fbbb
Chase <chasegarrett@tutanota.com>
2023-08-14 03:20:44 -0500

    added basics for questions

--- index.js
+++ index.js
@@ -4,7 +4,42 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Enter a description of your project"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Enter installation instructions",
+        default: "N/A"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Enter usage information",
+        default: "N/A"
+    },
+    {
+        type: "input",
+        name: "credits",
+        message: "Enter credits and/or contributions",
+        default: "N/A"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Choose a license for your project",
+        choices: ["MIT", "Apache", "GPL", "BSD", "None"]
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
